06414e3001f25303502cb72808f50a569b06fcc4
crt26 <callumturino@hotmail.co.uk>
2024-07-03 16:00:25 +0100

    restored cross to source code directory

---
+++ src/cross/Reference_Implementation/include/api.h
@@ -0,0 +1,63 @@
+/**
+ *
+ * Reference ISO-C11 Implementation of CROSS.
+ *
+ * @version 1.1 (March 2023)
+ *
+ * @author Alessandro Barenghi <alessandro.barenghi@polimi.it>
+ * @author Gerardo Pelosi <gerardo.pelosi@polimi.it>
+ *
+ * This code is hereby placed in the public domain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ''AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+
+
+#pragma once
+
+#include "parameters.h"
+#include "CROSS.h"
+
+#define CRYPTO_ALGNAME "CROSS"
+
+/*  no. of bytes of the secret key */
+#define CRYPTO_SECRETKEYBYTES (sizeof(prikey_t))
+
+/*  no. of bytes of the public key */
+#define CRYPTO_PUBLICKEYBYTES (sizeof(pubkey_t))
+
+/* no. of bytes of overhead in a signed message */
+#define CRYPTO_BYTES (sizeof(sig_t))
+
+/* required bytes of input randomness */
+#define  CRYPTO_RANDOMBYTES (SEED_LENGTH_BYTES) //CROSS library -  parameters.h
+
+
+int crypto_sign_keypair(unsigned char *pk,
+                        unsigned char *sk
+                       );
+
+int crypto_sign(unsigned char *sm,
+                unsigned long long *smlen,
+                const unsigned char *m,
+                unsigned long long mlen,
+                const unsigned char *sk
+               );
+
+int crypto_sign_open(unsigned char *m,
+                     unsigned long long *mlen,
+                     const unsigned char *sm,
+                     unsigned long long smlen,
+                     const unsigned char *pk
+                    );
