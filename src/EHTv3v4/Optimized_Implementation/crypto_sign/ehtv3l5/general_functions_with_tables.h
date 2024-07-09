4d6ea9e84cff67fdbea3379de3ff3af726d8738e
crt26 <callumturino@hotmail.co.uk>
2024-07-09 17:01:34 +0100

    Added EHTv3v4 to source code directory

---
+++ src/EHTv3v4/Optimized_Implementation/crypto_sign/ehtv3l5/general_functions_with_tables.h
@@ -0,0 +1,9 @@
+#ifndef general_functions_with_tables_h
+#define general_functions_with_tables_h
+
+unsigned char inverse(int a);
+unsigned char prod(int a, int b);
+unsigned char add(int a, int b);
+unsigned char sub(int a, int b);
+
+#endif
