b229e119d249a33ac4b1da48c3262ceb6b3d316f
billbuchanan <w.buchanan@napier.ac.uk>
2024-07-10 16:47:26 +0000

    Update

--- nist/HPPC/Reference_Implementation/HPPC128/go.bat
+++ nist/HPPC/Reference_Implementation/HPPC128/go.bat
@@ -1,2 +1,2 @@
 gcc -c *.c -I  C:\msys64\home\Administrator\m4ri
-gcc   -o hppc1.exe *.o -L C:\msys64\mingw64\lib\libflint.dll.a -L C:\msys64\mingw64\lib\libgmp.a -llibcrypto -llibssl
+gcc   -o hppc1.exe *.o -L libflint.dll.a -L libgmp.a -llibcrypto -llibssl
