5b703b678520f07bd21efd80fe5934aeeb9fe062
Vladislav Chesnokov <Vladislav.Chesnokov@6ghi.net>
2024-08-05 14:43:06 +0200

    Workaround with .CSV file format + TODO comment section + readme for scripts

--- scripts/main.go
+++ scripts/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
+// Workaround in project dir
 type PathVariables struct {
 	RootDir          string
 	SrcDir           string
@@ -28,7 +31,18 @@ type ConfigParams struct {
 	NumRuns uint8
 }
 
-const ResultsFileName = "sig_speed_results_run"
+type AlgorithmVariationMessage struct {
+	Keygen         string
+	Sign           string
+	Verify         string
+	PrivateKeySize uint
+	PublicKeySize  uint
+	SignatureSize  uint
+}
+
+// Used for internal channel communications
+// :Algorithm mame - [key -> value] :Algorithm output data
+type AlgorithmMessage map[string][]byte
 
 const HUFUWarningMessage = "WARNGING! - HUFU benchmarking takes a considerable amount of time to complete, even on a high performance machine"
 const HUFUSuggestMessage = "Would you like to include HUFU in the benchmarking? (y/n): "
@@ -40,8 +54,8 @@ const NumRunsMessage = "Enter the number of runs to be performed: "
 
 const sep = string(os.PathSeparator)
 
+// Set absolute path to root folder of the project (run only inside of scripts/)
 func NewPathVariables() (*PathVariables, error) {
-	// Set absolute path to root folder of the project (run only inside of scripts/)
 	wd, err := os.Getwd()
 	if err != nil {
 		fmt.Printf("Working dir error %s \n", err)
@@ -60,7 +74,10 @@ func NewPathVariables() (*PathVariables, error) {
 	return pvb, nil
 }
 
+// Extraction of algorithms and their versions from .txt files in test_data/alg_variation_lists dir
 func NewAlgVariationArrays(wd string) (map[string][]string, int) {
+	// :map[string][]string - dictionary of algorithm names with their versions
+	// :int - total amount of algorithm versions for a buffer size
 	var totalAmount int = 0
 
 	res := make(map[string][]string)
@@ -82,6 +99,7 @@ func NewAlgVariationArrays(wd string) (map[string][]string, int) {
 		for scanner.Scan() {
 			lines = append(lines, scanner.Text())
 		}
+		// :TODO Ascon_sign insted of Ascon_Sign?
 		res[info.Name()[:len(info.Name())-15]] = lines
 		totalAmount += len(lines)
 		return nil
@@ -92,7 +110,8 @@ func NewAlgVariationArrays(wd string) (map[string][]string, int) {
 	return res, totalAmount
 }
 
-func Config() *ConfigParams {
+// Set config of heavy params
+func setConfig() *ConfigParams {
 	var message string
 	var nums uint8
 	cfg := ConfigParams{}
@@ -133,8 +152,8 @@ func Config() *ConfigParams {
 	return &cfg
 }
 
+// Delete folder if its exists
 func deleteExistingFolder(wd string, name string) {
-	// Delete folder if its exists
 	err := filepath.Walk(wd, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("Working dir error %s \n", err)
@@ -152,41 +171,72 @@ func deleteExistingFolder(wd string, name string) {
 
 }
 
-// handleOutputChan handles the output channel and closes when producer stops
-func handleOutputChan(f *os.File, output chan []byte, stopchan chan bool) {
+// Convertion of output
+func extractRelevantContent(input string, excludeKey string) string {
+	// :input - working string
+	// :excludeKey - string above exclusion
+	lines := strings.Split(input, "\n")
+
+	// Iterate through the lines to find key to exclude
+	for i, line := range lines {
+		if strings.HasPrefix(line, excludeKey) {
+			return strings.Join(lines[:i+1], "\n")
+		}
+	}
+	return input
+}
+
+// handleOutputChan-writing from a buff chan into txt and csv files
+func handleOutputChan(f *os.File, f1 *os.File, output <-chan AlgorithmMessage, stopchan <-chan struct{}) {
+	defer f1.Close()
 	defer f.Close()
+	wCSV := csv.NewWriter(f1)
 	for {
 		select {
-		case line, ok := <-output:
+		case message, ok := <-output:
 			if !ok {
 				return
 			}
-			_, err := f.WriteString(string(line))
-			if err != nil {
-				fmt.Printf("File error %s \n", err)
-				continue
+			for _, data := range message {
+				res := extractRelevantContent(string(data), "Verify")
+				_, err := f.WriteString(res + "\n")
+				if err != nil {
+					fmt.Printf("TXT file error %s \n", err)
+					continue
+				}
+
+				err1 := wCSV.Write(strings.Split(res, "\n"))
+				if err1 != nil {
+					fmt.Printf("CSV file error %s \n", err1)
+				}
 			}
 		case <-stopchan:
+			wCSV.Flush()
 			return
 		}
 	}
 }
 
-func IterateAndRunAlgVers(BinDir string, k string, v []string, output chan<- ([]byte)) {
+// Iterating over the algorithm versions and send command output to the channel
+func IterateAndRunAlgVers(BinDir string, k string, v []string, output chan<- AlgorithmMessage) {
 	for _, element := range v {
 		out, err := exec.Command(filepath.Join(BinDir, k, "pqcsign_"+element)).Output()
 		if err != nil {
-			fmt.Printf("Command execution error: %s \nOutput:  %s \n", err, out)
+			fmt.Printf("Command execution error: %s \nOutput:  %s \nAlgorithm: %s \n", err, out, element)
 			continue
 		}
-		output <- out
+		output <- AlgorithmMessage{k: out}
 	}
 }
 
-// PerformBenchmarking performs the benchmarking
-func PerformBenchmarking(res map[string][]string, cfg *ConfigParams, pathvars *PathVariables, step int, output chan ([]byte), wg *sync.WaitGroup) {
+// PerformBenchmarking iterates over the algorithms and start goroutines for evaluation of each algorithm
+func PerformBenchmarking(res map[string][]string, cfg *ConfigParams, pathvars *PathVariables, step int, output chan AlgorithmMessage, wg *sync.WaitGroup) {
 	fmt.Printf("Performing step %s \n", strconv.Itoa(step+1))
 	for k, v := range res {
+		// Heavy algorithms
+		if (k == "HuFu" && !cfg.HUFU) || (k == "SNOVA" && !cfg.SNOVA) {
+			continue
+		}
 		wg.Add(1)
 		fmt.Printf("Algorithm %s execution started \n", k)
 		go func(k string, v []string) {
@@ -198,27 +248,34 @@ func PerformBenchmarking(res map[string][]string, cfg *ConfigParams, pathvars *P
 }
 
 func main() {
+	// Setting up of the WaitGroup to track all the goroutines
 	wg := sync.WaitGroup{}
 	start := time.Now()
+	// Set up the config
 	pathvars, err := NewPathVariables()
 	if err != nil {
 		panic(err)
 	}
 	res, amount := NewAlgVariationArrays(pathvars.AlgVariationsDir)
-	cfg := Config()
-	output := make(chan ([]byte), amount)
-	stopchan := make(chan bool)
+	cfg := setConfig()
+	// Setting up bufferend channel for communication and stop-signal channel
+	output := make(chan (AlgorithmMessage), amount)
+	stopchan := make(chan struct{})
+
 	fmt.Println("Started")
 	for run := 0; run < int(cfg.NumRuns); run++ {
 		file, err := os.Create(pathvars.ResultsDir + sep + "results" + strconv.Itoa(run+1) + "_" + time.Now().Local().Format("20060102") + ".txt")
-		if err != nil {
+		file1, err1 := os.Create(pathvars.ResultsDir + sep + "results" + strconv.Itoa(run+1) + "_" + time.Now().Local().Format("20060102") + ".csv")
+		if err != nil || err1 != nil {
 			fmt.Printf("File error %s \n", err)
 			return
 		}
-		go handleOutputChan(file, output, stopchan)
+		go handleOutputChan(file, file1, output, stopchan)
 		PerformBenchmarking(res, cfg, pathvars, run, output, &wg)
 	}
 	wg.Wait()
-	stopchan <- true
+	// Send close signals to both channels
+	close(output)
+	close(stopchan)
 	fmt.Printf("Finished in %s\n", time.Since(start))
 }
---
+++ scripts/readme.md
@@ -0,0 +1,21 @@
+# Scripts execution
+> [!IMPORTANT]
+> Run ./setup in root directory before run | Run all the scripts in scripts/ directory
+## Golang requirements
+- Install Go: Ensure you have Go installed. You can download it from the official [Go website](https://go.dev/dl/) (ver 1.22.5)
+- Set Up Your Workspace: It's common to set your **GOPATH**
+- Build the project as executable:
+```
+go build -o output_filename
+```
+Or run it directly:
+```
+go run main.go
+```
+## Current features
+- Algorithm results output in .csv and .txt formats
+- Parallel thread-safe algorithm execution
+
+## Future Enhancements
+- JSON conversion and formatting
+- HTML+JS dashboard
