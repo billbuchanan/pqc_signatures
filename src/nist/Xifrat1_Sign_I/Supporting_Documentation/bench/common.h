90e0b771ebf89d763be6fa4bef8ffe0594d2add1
crt26 <callumturino@hotmail.co.uk>
2024-09-10 19:41:47 +0100

    Removes reduant files in src

--- src/nist/Xifrat1_Sign_I/Supporting_Documentation/bench/common.h
+++
@@ -1,5 +0,0 @@
-/* DannyNiu/NJF, 2021-03-30. Public Domain. */
-
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
--- src/nist/Xifrat1_Sign_I/Supporting_Documentation/scripts/QGGen-v2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-src=QGGen-v2.c
-. ./QGGen.sh.inc
